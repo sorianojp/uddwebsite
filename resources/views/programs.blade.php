690b2efb943fa7a2a41ef31328df81e6449bd193
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2023-03-29 17:13:41 +0800

    update

--- resources/views/programs.blade.php
+++ resources/views/programs.blade.php
@@ -2,21 +2,6 @@
 @section('content')
 <div class="container-fluid">
  <div class="row my-5 mx-5">
-    <div class="col-sm-3">
-        <h3>News</h3>
-        @foreach ($news as $n)
-        <div class="row my-3 border-bottom mr-2">
-            <div class="col-sm-6 p-0">
-                <img src="/image/{{ $n->image }}" class="img-fluid">
-            </div>
-            <div class="col-sm-6">
-                <a href="{{ route('news.show', $n->id) }}"> <span class="font-weight-bold text-xs">{{ $n->title }}</span></a><br>
-                <span><i class="fa-regular fa-user text-dark"></i> {{ $n->user->name }}</span><br>
-                <span><i class="fa-regular fa-calendar text-dark"></i> {{ $n->created_at->format('Y-m-d') }}<p>
-            </div>
-        </div>
-        @endforeach
-    </div>
     <div class="col-sm-6">
         <h3>Programs Offerings</h3>
         <div class="accordion">
@@ -39,6 +24,21 @@
 
             @endforeach
         </div>
+    </div>
+    <div class="col-sm-3">
+        <h3>News</h3>
+        @foreach ($news as $n)
+        <div class="row my-3 border-bottom mr-2">
+            <div class="col-sm-6 p-0">
+                <img src="/image/{{ $n->image }}" class="img-fluid">
+            </div>
+            <div class="col-sm-6">
+                <a href="{{ route('news.show', $n->id) }}"> <span class="font-weight-bold text-xs">{{ $n->title }}</span></a><br>
+                <span><i class="fa-regular fa-user text-dark"></i> {{ $n->user->name }}</span><br>
+                <span><i class="fa-regular fa-calendar text-dark"></i> {{ $n->created_at->format('Y-m-d') }}<p>
+            </div>
+        </div>
+        @endforeach
     </div>
     <div class="col-sm-3">
         <h3>Events</h3>
