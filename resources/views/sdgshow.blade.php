731a6065aec0c3f906b0f881b777a921d7dff5f7
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-10-29 14:55:16 +0800

    update

--- resources/views/sdgshow.blade.php
+++ resources/views/sdgshow.blade.php
@@ -1,43 +1,32 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container-fluid">
-        <div class="row my-5 mx-5">
-            <div class="col-sm-12">
-                <h5 class="text-uppercase bg-primary p-2 rounded-lg">{{ $category->name }} | News and Events</h5>
-                <div class="row">
-                    @forelse ($category->news as $n)
-                        <div class="col-sm-6 col-md-4 col-lg-3">
-                            <div>
-                                <img src="/image/{{ $n->image }}" class="img-fluid rounded-top">
-                            </div>
-                            <div class="text-truncate">
-                                <a href="{{ route('news.show', $n->id) }}">{{ $n->title }}</a>
-                            </div>
-                        </div>
-                    @empty
-                        <div class="border p-2 my-1 mx-1 bg-secondary text-white">
-                            <span>No news in this category</span>
-                        </div>
-                    @endforelse
+    <section class="p-5">
+        <h5 class="text-uppercase bg-primary p-2 rounded-lg">{{ $category->name }} | News and Events</h5>
+        <div class="row">
+            @forelse ($category->news as $n)
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <img src="/image/{{ $n->image }}" class="img-fluid rounded-top">
+                    </div>
+                    <div class="text-truncate">
+                        <a href="{{ route('news.show', $n->id) }}">{{ $n->title }}</a>
+                    </div>
                 </div>
-
-                <div class="row">
-                    @forelse ($category->events as $e)
-                        <div class="col-sm-6 col-md-4 col-lg-3">
-                            <div>
-                                <img src="/image/{{ $e->image }}" class="img-fluid rounded-top">
-                            </div>
-                            <div class="text-truncate">
-                                <a href="{{ route('events.show', $e->id) }}">{{ $e->title }}</a>
-                            </div>
-                        </div>
-                    @empty
-                        <div class="border p-2 my-1 mx-1 bg-secondary text-white">
-                            <span>No events in this category</span>
-                        </div>
-                    @endforelse
-                </div>
-            </div>
+            @empty
+            @endforelse
         </div>
-    </div>
+        <div class="row">
+            @forelse ($category->events as $e)
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <img src="/image/{{ $e->image }}" class="img-fluid rounded-top">
+                    </div>
+                    <div class="text-truncate">
+                        <a href="{{ route('events.show', $e->id) }}">{{ $e->title }}</a>
+                    </div>
+                </div>
+            @empty
+            @endforelse
+        </div>
+    </section>
 @endsection
