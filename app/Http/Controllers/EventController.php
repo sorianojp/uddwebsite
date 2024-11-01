2a32f1df616ccb0a20308f071c16b67e90b0f7a5
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-01 11:41:09 +0800

    update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -41,7 +41,7 @@ class EventController extends Controller
             'title' => 'required',
             'content' => 'required',
             'category_id' => 'nullable',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5120',
         ]);
 
         $input = $request->all();
