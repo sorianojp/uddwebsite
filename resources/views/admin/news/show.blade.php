a521d91999e419f4b1bda21be4e32c0ff4936a2e
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-09 11:35:33 +0800

    update

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -9,23 +9,25 @@
 @endsection
 @section('content')
     <div class="container">
-        <div>
-            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('news.show', $news->id)) }}"
-                target="_blank">
-                <i class="bi bi-facebook h2"></i>
-            </a>
-        </div>
         <div class="row justify-content-center my-5">
             <div class="col-sm-8">
                 <h2 class="font-weight-bold">{{ $news->title }}</h2>
-                <span>{{ $news->user->name }} | {{ $news->created_at->format('F j, Y') }}</span> |
-                @if ($news->category)
-                    <a href="{{ route('sdgs.show', $news->category->id) }}">
-                        <span>{{ $news->category->name }}</span>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <span>{{ $news->user->name }} | {{ $news->created_at->format('F j, Y') }}</span> |
+                        @if ($news->category)
+                            <a href="{{ route('sdgs.show', $news->category->id) }}">
+                                <span>{{ $news->category->name }}</span>
+                            </a>
+                        @else
+                            <span>N/A</span>
+                        @endif
+                    </div>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('news.show', $news->id)) }}"
+                        target="_blank">
+                        <i class="bi bi-facebook h2"></i>
                     </a>
-                @else
-                    <span>N/A</span>
-                @endif
+                </div>
                 <div class="image-container">
                     <img src="/image/{{ $news->image }}" class="img-fluid">
                 </div>
