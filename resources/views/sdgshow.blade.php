49ae2be908b310bf35ee20bd0d6389c4e0e0a878
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-09 11:49:21 +0800

    update

--- resources/views/sdgshow.blade.php
+++ resources/views/sdgshow.blade.php
@@ -5,11 +5,22 @@
         <div class="row">
             @forelse ($category->news as $n)
                 <div class="col-sm-6 col-md-4 col-lg-3">
-                    <div>
-                        <img src="/image/{{ $n->image }}" class="img-fluid rounded-top">
-                    </div>
-                    <div class="text-truncate">
-                        <a href="{{ route('news.show', $n->id) }}">{{ $n->title }}</a>
+                    <div class="card">
+                        <div class="image-container-xs">
+                            <img src="/image/{{ $n->image }}" class="card-img-top" alt="{{ $n->title }}">
+                        </div>
+                        <div class="card-body">
+                            <a href="{{ route('news.show', $n->id) }}">
+                                <h5 class="card-title text-truncate">{{ $n->title }}</h5>
+                            </a>
+                        </div>
+                        <div class="card-footer bg-primary text-white d-flex justify-content-between align-items-center">
+                            <span>{{ $n->created_at->format('F j, Y') }}</span>
+                            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('news.show', $n->id)) }}"
+                                target="_blank" class="text-white">
+                                <i class="bi bi-facebook h3"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             @empty
@@ -18,11 +29,22 @@
         <div class="row">
             @forelse ($category->events as $e)
                 <div class="col-sm-6 col-md-4 col-lg-3">
-                    <div>
-                        <img src="/image/{{ $e->image }}" class="img-fluid rounded-top">
-                    </div>
-                    <div class="text-truncate">
-                        <a href="{{ route('events.show', $e->id) }}">{{ $e->title }}</a>
+                    <div class="card">
+                        <div class="image-container-xs">
+                            <img src="/image/{{ $e->image }}" class="card-img-top" alt="{{ $e->title }}">
+                        </div>
+                        <div class="card-body">
+                            <a href="{{ route('events.show', $e->id) }}">
+                                <h5 class="card-title text-truncate">{{ $e->title }}</h5>
+                            </a>
+                        </div>
+                        <div class="card-footer bg-primary text-white d-flex justify-content-between align-items-center">
+                            <span>{{ $e->created_at->format('F j, Y') }}</span>
+                            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('events.show', $e->id)) }}"
+                                target="_blank" class="text-white">
+                                <i class="bi bi-facebook h3"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             @empty
