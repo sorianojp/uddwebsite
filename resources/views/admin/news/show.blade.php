461954fefcec346f90af8f2a1f55d6907bb32ee8
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-09 11:36:38 +0800

    update

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -11,22 +11,24 @@
     <div class="container">
         <div class="row justify-content-center my-5">
             <div class="col-sm-8">
-                <h2 class="font-weight-bold">{{ $news->title }}</h2>
-                <div class="d-flex justify-content-between align-items-center">
+                <h2 class="font-weight-bold">{{ $news->title }}</h2><br>
+                @if ($news->category)
+                    <a href="{{ route('sdgs.show', $news->category->id) }}">
+                        <span>{{ $news->category->name }}</span>
+                    </a>
+                @else
+                    <span>N/A</span>
+                @endif
+                <div class="d-flex justify-content-between align-items-center my-4">
                     <div>
                         <span>{{ $news->user->name }} | {{ $news->created_at->format('F j, Y') }}</span> |
-                        @if ($news->category)
-                            <a href="{{ route('sdgs.show', $news->category->id) }}">
-                                <span>{{ $news->category->name }}</span>
-                            </a>
-                        @else
-                            <span>N/A</span>
-                        @endif
                     </div>
-                    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('news.show', $news->id)) }}"
-                        target="_blank">
-                        <i class="bi bi-facebook h2"></i>
-                    </a>
+                    <div>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('news.show', $news->id)) }}"
+                            target="_blank">
+                            <i class="bi bi-facebook h2"></i>
+                        </a>
+                    </div>
                 </div>
                 <div class="image-container">
                     <img src="/image/{{ $news->image }}" class="img-fluid">
