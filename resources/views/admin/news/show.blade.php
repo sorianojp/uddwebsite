7f303f275e1673f764540db9242a05a4fadb64b2
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-09 11:32:48 +0800

    update

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
+@section('head')
+    <!-- Open Graph meta tags for Facebook sharing -->
+    <meta property="og:title" content="{{ $event->title }}" />
+    <meta property="og:description" content="{{ Str::limit(strip_tags($event->content), 150) }}" />
+    <meta property="og:image" content="{{ asset('image/' . $event->image) }}" />
+    <meta property="og:url" content="{{ route('events.show', $event->id) }}" />
+    <meta property="og:type" content="article" />
+@endsection
 @section('content')
     <div class="container">
+        <div>
+            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('events.show', $event->id)) }}"
+                target="_blank">
+                <i class="bi bi-facebook h2"></i>
+            </a>
+        </div>
         <div class="row justify-content-center my-5">
             <div class="col-sm-8">
                 <h2 class="font-weight-bold">{{ $news->title }}</h2>
