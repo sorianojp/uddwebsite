c5a6c67b6ba58ea240b50515e786f07b30a1f294
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-09 11:33:27 +0800

    update

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 @section('head')
     <!-- Open Graph meta tags for Facebook sharing -->
-    <meta property="og:title" content="{{ $event->title }}" />
-    <meta property="og:description" content="{{ Str::limit(strip_tags($event->content), 150) }}" />
-    <meta property="og:image" content="{{ asset('image/' . $event->image) }}" />
-    <meta property="og:url" content="{{ route('events.show', $event->id) }}" />
+    <meta property="og:title" content="{{ $news->title }}" />
+    <meta property="og:description" content="{{ Str::limit(strip_tags($news->content), 150) }}" />
+    <meta property="og:image" content="{{ asset('image/' . $news->image) }}" />
+    <meta property="og:url" content="{{ route('news.show', $news->id) }}" />
     <meta property="og:type" content="article" />
 @endsection
 @section('content')
     <div class="container">
         <div>
-            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('events.show', $event->id)) }}"
+            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('news.show', $event->id)) }}"
                 target="_blank">
                 <i class="bi bi-facebook h2"></i>
             </a>
