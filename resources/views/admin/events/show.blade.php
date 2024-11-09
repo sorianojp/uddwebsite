81f5d305dc0e1cc7badd2dccba9892785e8f1e1c
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-09 10:53:43 +0800

    update

--- resources/views/admin/events/show.blade.php
+++ resources/views/admin/events/show.blade.php
@@ -4,22 +4,25 @@
     <meta property="og:title" content="{{ $event->title }}" />
     <meta property="og:description" content="{{ Str::limit(strip_tags($event->content), 150) }}" />
     <meta property="og:image" content="{{ asset('image/' . $event->image) }}" />
-    <meta property="og:url" content="{{ route('event.show', $event->id) }}" />
+    <meta property="og:url" content="{{ route('events.show', $event->id) }}" />
     <meta property="og:type" content="article" />
 @endsection
 @section('content')
     <div class="container">
         <div class="row justify-content-center my-5">
             <div class="col-sm-8">
-                <h2 class="font-weight-bold">{{ $event->title }}</h2>
-                <span>{{ $event->user->name }} | {{ $event->created_at->format('F j, Y') }}</span> |
-                @if ($event->category)
-                    <a href="{{ route('sdgs.show', $event->category->id) }}">
-                        <span class="badge badge-secondary">{{ $event->category->name }}</span>
-                    </a>
-                @else
-                    <span>N/A</span>
-                @endif
+                <div class="row">
+                    <h2 class="font-weight-bold">{{ $event->title }}</h2>
+                    @if ($event->category)
+                        <a href="{{ route('sdgs.show', $event->category->id) }}">
+                            <span class="badge badge-secondary">{{ $event->category->name }}</span>
+                        </a>
+                    @else
+                        <span>N/A</span>
+                    @endif
+                </div>
+                <h4 class="font-weight-bold">{{ $event->user->name }}</h4>
+                <h5>{{ $event->created_at->format('F j, Y') }}</h5>
                 <div class="my-3">
                     <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('events.show', $event->id)) }}"
                         target="_blank" class="btn btn-primary">
