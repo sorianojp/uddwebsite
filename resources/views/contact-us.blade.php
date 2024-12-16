1c08dccf8a9b212d6396db4a2a66eed4917d6135
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-12-16 15:18:49 +0800

    update

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -1,33 +1,48 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container min-vh-100 d-flex justify-content-center align-items-center">
+    <div class="container my-5">
+        <h1 class="text-primary text-center my-5">Contact Us</h1>
         <div class="row text-center w-100">
-            <div class="col-sm-3 d-flex">
-                <div class="bg-primary text-white p-5 w-100 h-100 d-flex flex-column justify-content-center">
+            <div class="col-sm-4 d-flex my-2">
+                <div class="bg-primary text-white p-5 w-100">
                     <i class="bi bi-geo-alt-fill h3"></i>
                     <h5>Address</h5>
                     <p>Pantal, Arellano St., Dagupan City, Philippines, 2400</p>
                 </div>
             </div>
-            <div class="col-sm-3 d-flex">
-                <div class="bg-primary text-white p-5 w-100 h-100 d-flex flex-column justify-content-center">
+            <div class="col-sm-4 d-flex my-2">
+                <div class="bg-primary text-white p-5 w-100">
                     <i class="bi bi-telephone-fill h3"></i>
                     <h5>Phone</h5>
                     <p>(075) 522 2405</p>
                 </div>
             </div>
-            <div class="col-sm-3 d-flex">
-                <div class="bg-primary text-white p-5 w-100 h-100 d-flex flex-column justify-content-center">
+            <div class="col-sm-4 d-flex my-2">
+                <div class="bg-primary text-white p-5 w-100">
                     <i class="bi bi-envelope-at-fill h3"></i>
                     <h5>E-Mail</h5>
                     <p>info@cdd.edu.ph</p>
                 </div>
             </div>
-            <div class="col-sm-3 d-flex">
-                <div class="bg-primary text-white p-5 w-100 h-100 d-flex flex-column justify-content-center">
+            <div class="col-sm-4 d-flex my-2">
+                <div class="bg-primary text-white p-5 w-100">
                     <i class="bi bi-facebook h3"></i>
                     <h5>Facebook</h5>
                     <a class="text-white" href="https://www.facebook.com/uddinfoboard/">Visit</a>
+                </div>
+            </div>
+            <div class="col-sm-4 d-flex my-2">
+                <div class="bg-primary text-white p-5 w-100">
+                    <i class="bi bi-person-video3 h3"></i>
+                    <h5>UdD Portal</h5>
+                    <a class="text-white" href="http://mycdd.cdd.edu.ph/PARENTS_STUDENTS/">Visit</a>
+                </div>
+            </div>
+            <div class="col-sm-4 d-flex my-2">
+                <div class="bg-primary text-white p-5 w-100">
+                    <i class="bi bi-person-workspace h3"></i>
+                    <h5>STEP Portal</h5>
+                    <a class="text-white" href="https://udd.steps.com.ph/login">Visit</a>
                 </div>
             </div>
         </div>
