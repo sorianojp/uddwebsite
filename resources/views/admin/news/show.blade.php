0d95ff2d47ba1d40fa6b2ea4553a6c07cb558caf
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-12-17 13:24:35 +0800

    update

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -32,8 +32,8 @@
                 <div>
                     <img src="/image/{{ $news->image }}" class="img-fluid">
                 </div>
-                <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout=""
-                    data-size=""><a target="_blank"
+                <div class="fb-share-button" data-href="{{ route('news.show', $news->id) }}" data-layout="" data-size="">
+                    <a target="_blank"
                         href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
                         class="fb-xfbml-parse-ignore">Share</a></div>
                 <div class="text-justify content-container">{!! $news->content !!}</div>
