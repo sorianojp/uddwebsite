338dfecd1200cb885377e8b77cec3ed2c27ee94d
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-12-17 12:52:38 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
         AOS.init();
     </script>
     @yield('scripts')
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v21.0">
+    </script>
 </body>
 
 </html>
